df4888001e765449606ad30f1a9b6a88dee64f40
sulejman1994 <100504478+sulejman1994@users.noreply.github.com>
2022-06-10 14:50:17 +0300

    Project 10

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -6,10 +6,6 @@
 #include <unordered_set>
 #include <set>
 
-// Класс RequestHandler играет роль Фасада, упрощающего взаимодействие JSON reader-а
-// с другими подсистемами приложения.
-// См. паттерн проектирования Фасад: https://ru.wikipedia.org/wiki/Фасад_(шаблон_проектирования)
-
 using transport_catalogue::TransportCatalogue, renderer::MapRenderer, std::string, std::unordered_set, std::set;
 
 namespace request_handler {
